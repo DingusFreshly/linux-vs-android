01d38900a1eb3f428e40b1c59692ec4d6c520009
Dingus <mobro4ever@yahoo.com>
2025-09-04 18:38:56 -0500

    helaaaaao

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,33 +1,47 @@
-package io.github.some_example_name; // BLUE CIRCLE TEST
+package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2; // ve
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.graphics.Color;
+
 
 public class Main extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
 
-    private Vector2 playerPos;
-    private Vector2 playerVel;
+    // private Vector2 playerPos; // todo: get rid of this crap
+    // private Vector2 playerVel;
     private final float RADIUS = 10f;
     private final float SPEED = 0.2f; // change ts so it doesnt be goofy i guess
     private final float DRAG = 0.95f;
-    private final float GRAVITY = -0.1f;
 
     private int SCREEN_WIDTH ; //holy its taking eons
     private int SCREEN_HEIGHT; // scre it im doing it mangually
-
+    private Vector2 GRAVITY;
+    public World world;
+    private Player player;
+    
     @Override
     public void create() {
+        //constant intit
+        GRAVITY = new Vector2(0, -0.1f);
         shapeRenderer = new ShapeRenderer();
-        playerPos = new Vector2((float)SCREEN_WIDTH / 2f, (float)SCREEN_HEIGHT / 2f);
-        playerVel = Vector2.Zero;
 
         SCREEN_WIDTH = Gdx.graphics.getWidth();
         SCREEN_HEIGHT = Gdx.graphics.getHeight();
+
+        // playerPos = new Vector2((float)SCREEN_WIDTH / 2f, (float)SCREEN_HEIGHT / 2f);
+        // playerVel = Vector2.Zero;
+
+        world = new World(GRAVITY, true); // world instantiate before player ‼️
+        player = new Player(world, new Vector2(5, 10));//holy crap its so tiny
+
     }
 
     @Override
@@ -35,40 +49,44 @@ public class Main extends ApplicationAdapter {
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // two updates to clear
 
-        handleInput();
-        movePlayer();
-        wrapPlayer();
-        drawShapes();//k i think this is why its not running
+        world.step(1/60f, 6, 2); 
+        
+        player.update();
+        // handleInput();
+        // movePlayer();
+        // wrapPlayer();
+        drawPlayers();
     }
 
-    private void handleInput() {
-        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.W)) playerVel.y += SPEED;
-        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.S)) playerVel.y -= SPEED;
-        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.A)) playerVel.x -= SPEED;
-        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.D)) playerVel.x += SPEED;
-    }
+    // private void handleInput() {
+    //     if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.W)) playerVel.y += SPEED;
+    //     if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.S)) playerVel.y -= SPEED;
+    //     if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.A)) playerVel.x -= SPEED;
+    //     if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.D)) playerVel.x += SPEED;
+    // }
     
-    private void movePlayer() { // actually apply vel to pos and add drag
-        playerVel.scl(DRAG);
-        playerVel.y += GRAVITY;
-        playerPos = playerPos.add(playerVel);
-    }
+    // private void movePlayer() { // actually apply vel to pos and add drag
+    //     playerVel.scl(DRAG);
+    //     playerPos = playerPos.add(playerVel);
+    // }
     
-    //where is screen size uhh
-    private void wrapPlayer() {
-        //particle accelarator
-        if (playerPos.x > SCREEN_WIDTH) playerPos.x = 0;
-        if (playerPos.x < 0) playerPos.x = SCREEN_WIDTH;
+    // //where is screen size uhh
+    // private void wrapPlayer() {
+    //     //particle accelarator
+    //     if (playerPos.x > SCREEN_WIDTH) playerPos.x = 0;
+    //     if (playerPos.x < 0) playerPos.x = SCREEN_WIDTH;
 
-        // if (playerPos.y > SCREEN_HEIGHT) playerPos.y = 0;
-        // if (playerPos.y < 0) playerPos.y = SCREEN_HEIGHT;
-        if (playerPos.y < RADIUS) playerPos.y = RADIUS;
-    }
+    //     // if (playerPos.y > SCREEN_HEIGHT) playerPos.y = 0;
+    //     // if (playerPos.y < 0) playerPos.y = SCREEN_HEIGHT;
+    //     if (playerPos.y < RADIUS) playerPos.y = RADIUS;
+    // }
     
-    private void drawShapes() {
+    private void drawPlayers() {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);//dam zuckerburg
         shapeRenderer.setColor(new Color(0, 0, 255, 0));
-        shapeRenderer.circle(playerPos.x, playerPos.y, RADIUS);
+
+        player.draw(shapeRenderer);
+
         shapeRenderer.end();
     }
 
---
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -0,0 +1,75 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2; // ve
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.graphics.Color;
+
+public class Player {
+    
+    private World world;
+    public Vector2 velocity;
+    private Color color;
+    public  PolygonShape box;
+    public Body body;
+
+    public static final float PIXELS_PER_METER = 50f;
+    public static final float PLAYER_SIZE = 1;
+    public static final float DRAW_SIZE = PLAYER_SIZE / PIXELS_PER_METER;
+
+    public Player(World world, Vector2 startPos) {
+        this.world = world; 
+        this.velocity = new Vector2(0, 0);
+        this.color = Color.PURPLE;
+
+        createBody(startPos);
+        // DOES THIS WORK
+    }
+
+    private void createBody(Vector2 startPos ) {
+        //idk what im doing lol
+        
+        BodyDef playerBodyDef = new BodyDef();
+        playerBodyDef.type = BodyType.DynamicBody;
+        playerBodyDef.position.set(startPos.x,startPos.y); 
+        // how to set position of player
+
+        body = world.createBody(playerBodyDef);
+
+        //create collider set the collider
+        box = new PolygonShape();
+        box.setAsBox(DRAW_SIZE, DRAW_SIZE); // rahhhhh DID YOU PUSH TO GIT
+
+        //ts is for physical properties
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = box;
+        fixtureDef.density = 1f;
+        fixtureDef.friction = 0.3f;
+        fixtureDef.restitution = 0.2f;
+        
+        body.createFixture(fixtureDef);
+        //gots to clear the collider now that its already atached
+        box.dispose();
+    }
+
+    public void draw(ShapeRenderer renderer) {
+        Vector2 pos = body.getPosition();
+        
+        renderer.setColor(color);
+        renderer.rect(
+            pos.x - PLAYER_SIZE / 2,
+            pos.y - PLAYER_SIZE  / 2,
+            PLAYER_SIZE, 
+            PLAYER_SIZE
+            
+            ); //maybe turn this to an image later
+    }
+
+    public void update() {
+        // ts is how u apply velocity to the bode
+        body.setLinearVelocity(velocity);
+    }//
+    
+    
+}
